54495d6f8dd0f6b4c76fd95f53cd993a4da82cd1
harshamaiya99 <harshamaiya99@gmail.com>
2025-11-12 20:37:27 +0530

    Rename function to set custom title for HTML report in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,3 +1,3 @@
-def html_report_title(report):
+def pytest_html_report_title(report):
     """Callback to set the custom title of the HTML report."""
     report.title = "API Test Run Report - Pytest"
